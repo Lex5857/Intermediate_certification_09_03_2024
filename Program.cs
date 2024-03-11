63e996c525a052906c7fb41f2d086daebbc74654
Алексей Порошкин <lex5857@rambler.ru>
2024-03-11 21:34:29 +0300

    Добавил решение задач

--- Program.cs
+++ Program.cs
@@ -15,3 +15,65 @@
 // Пример:
 // [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1
 
+void ShowNumbersFromMtoN(int m, int n)
+{
+    if (m == n)
+    {
+        Console.Write($"{m} ");
+        return;
+    }
+    Console.Write($"{m}, ");
+    if (m > n)
+    {
+        ShowNumbersFromMtoN(m - 1, n);;
+    }
+    else
+    {
+        ShowNumbersFromMtoN(m + 1, n);
+    }
+}
+
+int Ackerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return Ackerman(m - 1, 1);
+    }
+    else if (m > 0 && n > 0)
+    {
+        return Ackerman(m - 1, Ackerman(m, n - 1));
+    }
+    else
+    {
+        return n + 1;
+    }
+}
+
+void ShowReversArray(int[] array, int endIndexArray)
+{
+    if (endIndexArray < 0)
+    {
+        return;
+    }
+    Console.Write($"{array[endIndexArray]} ");
+    ShowReversArray(array, endIndexArray - 1);
+}
+
+int m = 1;
+int n = 4;
+Console.Clear();
+
+Console.Write("Задача 1: ");
+ShowNumbersFromMtoN(m, n);
+Console.WriteLine();
+Console.Write("Задача 2: ");
+Console.Write(Ackerman(m, n));
+Console.WriteLine();
+Console.Write("Задача 3: ");
+int[] array = {1, 2, 5, 0, 10, 34};
+int endIndexArray = array.Length - 1;
+ShowReversArray(array, endIndexArray);
